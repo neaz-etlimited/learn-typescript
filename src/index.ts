344ceb1fbf40f8cd4be45f994be7147624801f29
Neaz Mahmud <neaz@etlimited.net>
2024-06-08 17:54:48 +0600

    Done methodoverride

--- src/index.ts
+++ src/index.ts
@@ -4,20 +4,27 @@ class Person {
     return this.firstName + "" + this.lastName;
   }
 
-  walk(){
-    console.log('Walking')
+  walk() {
+    console.log("Walking");
   }
 }
 
-
 class Student extends Person {
-    constructor(public studentId: number, firstName: string, lastName: string){
-        super(firstName, lastName)
-    }
+  constructor(public studentId: number, firstName: string, lastName: string) {
+    super(firstName, lastName);
+  }
 
-    takeTest(){
-        console.log("Taking")
-    }
+  takeTest() {
+    console.log("Taking");
+  }
 }
 
-let student = new Student (1, 'John', 'john@gmail.com');
+class Teacher extends Person {
+  override get fullName() {
+    return "Professor " + super.fullName;
+  }
+}
+
+let teacher = new Teacher("John", "Smith");
+
+console.log(teacher.fullName);
