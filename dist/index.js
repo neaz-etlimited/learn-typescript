a9b696148a84202acac9c8948716656a1a310833
Neaz Mahmud <neaz@etlimited.net>
2024-06-09 14:33:24 +0600

    Done method decorator

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,20 @@
-function factorial_recurse(num) {
-  if (num === 0 || num === 1) {
-    return 1;
-  } else return num * factorial_recurse(num - 1);
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+function Component(constructor) {
+    console.log("Component Decorator called");
+    constructor.prototype.uniqueId = Date.now();
+    constructor.prototype.insertInDOM = () => {
+        console.log("Inserting the component in the DOM");
+    };
 }
-
-console.log(factorial_recurse(5));
+let ProfileComponent = class ProfileComponent {
+};
+ProfileComponent = __decorate([
+    Component
+], ProfileComponent);
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,90 +1,46 @@
-// class KeyValuePair<K, V> {
-//     constructor(public key: K, public value: V){
+// Decorators
+// Class decorator
 
+// function Component(constructor: Function) {
+//   console.log("Component Decorator called");
+//   constructor.prototype.uniqueId = Date.now();
+//   constructor.prototype.insertInDOM = () => {
+//     console.log("Inserting the component in the DOM");
+//   };
+// }
+
+// @Component
+// class ProfileComponent {}
+
+// Parameterized Decorator
+
+// function Component(value: number){
+//     return (constructor: Function) => {
+//         console.log('Component decorator called')
 //     }
 // }
 
-// let pair = new KeyValuePair<string, string>('1', 'a');
-
-// class ArrayUtils {
-//   static wrapInArray<T>(value: T) {
-//     return [value];
-//   }
+// Decorator Composition
+// function Pipe(constructor: Function){
+//     console.log('Pipe decorator called');
+//     constructor.prototype.pipe = true;
 // }
 
-// let utils = new ArrayUtils();
-
-// let numbers = ArrayUtils.wrapInArray(1);
-
-// interface Result<T> {
-//   data: T | null;
-//   error: string | null;
-// }
-
-// function fetch<T>(url: string): Result<T> {
-//   return { data: null, error: null };
-// }
-
-// interface User {
-//   username: string;
-// }
-
-// interface Product {
-//   title: string;
-// }
-
-// let result = fetch<User>("url");
-
-// interface Product{
-//     name: string;
-//     price: number;
-// }
-
-// class Store<T>{
-//     protected _objects: T[] = [];
-
-//     add(obj: T): void {
-//       this._objects.push(obj);
-//     }
-// }
-
-
-// class CompressibleStore<T> extends Store<T> {
-//     compress(){}
-// }
-
-// // Restricting the generic type parameter
-// class SearchableStore<T extends { name : string}> extends Store<T> {
-//     find(name: string): T | undefined {
-//         return this._objects.find((obj)=> obj.name === name)
-//     }
-// }
-
-// class ProductStore extends Store<Product> {
-//         filterByCategory(category)
-// }
-
-
-Type mapping
-
-interface Product {
-    name : string;
-    price: number;
+// Methods Decorator
+function Log(target: any, methodName: string, descriptor: PropertyDescriptor) {
+  const original = descriptor.value as Function;
+  descriptor.value = function (...args: any) {
+    console.log("Before");
+    original.call(this, ...args);
+    console.log("After");
+  };
+}
+class Person {
+  @Log
+  say(message: string) {
+    console.log("Person says " + message);
+  }
 }
 
-type ReadOnly<T> = {
-   readonly [K in keyof T] : T[K]
-}
-
-type Optional<T> = {
-    [K in keyof T]?: T[K]
-}
-    
-type Nullable<T> = {
-    [K in keyof T]: T[K] | null
-}
-
-let product: ReadOnly<Product> = {
-    name : 'a',
-    price : 1
-}
+let person = new Person();
+person.say("Hello");
