7ecca4d438e693a8115fb04f0c877ac31e8cd4fe
Neaz Mahmud <neaz@etlimited.net>
2024-06-09 14:45:27 +0600

    Done accessor decorator

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,24 @@
-// Decorators
-// Class decorator
-
-// function Component(constructor: Function) {
-//   console.log("Component Decorator called");
-//   constructor.prototype.uniqueId = Date.now();
-//   constructor.prototype.insertInDOM = () => {
-//     console.log("Inserting the component in the DOM");
-//   };
-// }
-
-// @Component
-// class ProfileComponent {}
-
-// Parameterized Decorator
-
-// function Component(value: number){
-//     return (constructor: Function) => {
-//         console.log('Component decorator called')
-//     }
-// }
-
-// Decorator Composition
-// function Pipe(constructor: Function){
-//     console.log('Pipe decorator called');
-//     constructor.prototype.pipe = true;
-// }
-
-// Methods Decorator
-function Log(target: any, methodName: string, descriptor: PropertyDescriptor) {
-  const original = descriptor.value as Function;
-  descriptor.value = function (...args: any) {
-    console.log("Before");
-    original.call(this, ...args);
-    console.log("After");
+// Accessor decorator
+function Capitalize(
+  target: any,
+  methodName: string,
+  descriptor: PropertyDescriptor
+) {
+  const original = descriptor.get;
+  descriptor.get = function () {
+    const result = original?.call(this);
+    return typeof result === "string" ? result.toUpperCase() : result;
   };
 }
+
 class Person {
-  @Log
-  say(message: string) {
-    console.log("Person says " + message);
+  constructor(public firstName: string, public lastName: string) {}
+
+  @Capitalize
+  get fullName() {
+    return null;
   }
 }
 
-let person = new Person();
-person.say("Hello");
+let person = new Person("mosh", "hamedani");
+console.log(person.fullName); // Output: MOSH HAMEDANI
